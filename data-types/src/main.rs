5714e227a1529285df233b69c615eba8e4a37f1b
Xharry01X <harshitsingh50621@gmailcom>
2024-10-20 23:34:01 +0530

    done basic data-types

---
+++ data-types/src/main.rs
@@ -0,0 +1,14 @@
+// this is the following repoisitory for undersatnbding database
+
+
+fn main(){
+    let number: i32 = 42;
+    let float: f64 = 3.2;
+    let boolean: char = "r";
+    let tuple: (i32, f64) = (10,20.5);
+
+    // you can give size
+    let array: [i32; 3] = [1,2,3];
+    println!("{} {} {} {} {}", number,float,boolean,tuple);
+}
+
---
+++ data-types/target/debug/deps/data_types-816701f5c1abc926.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/ASUS/documents/github/rust/data-types/target/debug/deps/data_types-816701f5c1abc926: src/main.rs
+
+/mnt/c/Users/ASUS/documents/github/rust/data-types/target/debug/deps/data_types-816701f5c1abc926.d: src/main.rs
+
+src/main.rs:
